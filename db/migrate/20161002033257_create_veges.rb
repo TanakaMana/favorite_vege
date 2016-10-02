e3abd7c73455254ee954ac8f6c80a6a9b253981b
田中 真奈 <tanakamana@tanaka-no-MacBook-Air.local>
2016-10-02 13:29:25 +0900

    migrate veges table

--- db/migrate/20161002033257_create_veges.rb
+++ db/migrate/20161002033257_create_veges.rb
@@ -1,7 +1,8 @@
 class CreateVeges < ActiveRecord::Migration
   def change
     create_table :veges do |t|
-
+      t.string :vege
+      t.text :image
       t.timestamps null: false
     end
   end
